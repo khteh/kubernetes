1f3e5367988eb3f13741a359ab7fdbc987818ac2
Teh Kok How <funcoolgeek@gmail.com>
2019-03-29 20:39:42 +0800

    Update restart_redis.sh

--- restart_redis.sh
+++ restart_redis.sh
@@ -3,4 +3,5 @@ kubectl scale sts redis-cluster --replicas=0
 kubectl delete cm redis-cluster-config --ignore-not-found=true
 kubectl delete sts redis-cluster
 kubectl delete pvc -l app=redis-cluster
+kubectl delete job redis-cluster-init
 kubectl apply -f redis-cluster-config.yml,redis-cluster.yml
