d825c6a72a24a9555ab0eb63e0052d22b373f014
Teh Kok How <khteh@KHTeh-T490.biz4x.com>
2019-07-24 15:20:02 +0800

    Add kube2iam, alb, ingress

--- biz4x/restart_biz4xbackend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#./restart_env.sh
-kubectl scale sts biz4x-backend --replicas=0
-kubectl delete secret biz4x-secret --ignore-not-found=true
-kubectl delete statefulset biz4x-backend
-kubectl delete svc svc-biz4xbackend
-kubectl apply -f biz4x-secret.yml,svc-biz4xbackend.yml,biz4x-backend.yml
---
+++ restart_alb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+kubectl delete deployment external-dns --ignore-not-found=true
+kubectl delete ds kube2iam -n kube-system --ignore-not-found=true
+kubectl delete ing biz4x-ingress --ignore-not-found=true
+kubectl delete deployment alb-ingress-controller -n kube-system --ignore-not-found=true
+kubectl apply -f kube2iam-daemonset.yml,rbac-role.yml,biz4x-service-account.yml,alb-ingress-controller.yml,ingress.yml
+#,external-dns-service-accounts.yml start this only AFTER the ALB is in active state. Otherwise it creates TXT record instead of CNAME
