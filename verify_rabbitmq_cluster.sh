ffe7cb64072c5cda123ef66b2c24930518f38f16
Teh Kok How <kokhow.teh@4xlabs.co>
2019-09-19 14:05:22 +0800

    Update rabbitmq verification script to check all nodes and verify plugin

--- verify_rabbitmq_cluster.sh
+++ verify_rabbitmq_cluster.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 FIRST_POD=$(kubectl get pods --namespace default -l 'app=rabbitmq' -o jsonpath='{.items[0].metadata.name }')
-kubectl exec --namespace=default $FIRST_POD rabbitmqctl cluster_status
+nodes=`kubectl get pods -l app=rabbitmq | grep rabbitmq | wc -l`
+echo $nodes nodes
+for i in $(seq 0 $(( --nodes )));
+do
+	echo rabbitmq-$i:
+	kubectl exec rabbitmq-$i rabbitmqctl cluster_status
+	kubectl exec rabbitmq-$i rabbitmq-plugins is_enabled rabbitmq_peer_discovery_k8s
+	echo
+done
