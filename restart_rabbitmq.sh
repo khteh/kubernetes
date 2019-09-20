9dd0e3fd7e43d1ea41bcf794a160299f6b9c17f2
Teh Kok How <kokhow.teh@4xlabs.co>
2019-09-20 11:10:41 +0800

    Add rabbitmq definitions

--- restart_rabbitmq.sh
+++ restart_rabbitmq.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 kubectl delete job rabbitmq-cluster-init --ignore-not-found=true
 kubectl scale sts rabbitmq --replicas=0
-kubectl delete cm rabbitmq-config --ignore-not-found=true
+kubectl delete cm rabbitmq-config rabbitmq-definitions --ignore-not-found=true
 kubectl delete sts rabbitmq --ignore-not-found=true
 kubectl delete sa svc-rabbitmq --ignore-not-found=true
 kubectl delete role svc-rabbitmq --ignore-not-found=true
 kubectl delete rolebinding svc-rabbitmq --ignore-not-found=true
-kubectl apply -f rabbitmq_rbac.yml,rabbitmq-config.yml,svc-rabbitmq.yml,rabbitmq.yml
+kubectl apply -f rabbitmq_rbac.yml,rabbitmq-config.yml,rabbitmq-definitions.yml,svc-rabbitmq.yml,rabbitmq.yml
