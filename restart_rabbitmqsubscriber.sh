ca9b39ddb129a36cff685e9bbf66779dd1b433a1
Teh Kok How <kokhow.teh@4xlabs.co>
2019-09-20 18:12:43 +0800

    Create 2 different subscribers - kern.* and *.critical

--- restart_rabbitmqsubscriber.sh
+++ restart_rabbitmqsubscriber.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #./restart_env.sh
-kubectl scale sts rabbitmq-subscriber --replicas=0
+kubectl scale sts kern-subscriber --replicas=0
+kubectl scale sts critical-subscriber --replicas=0
 kubectl delete secret rabbitmq-secret --ignore-not-found=true
-kubectl delete statefulset rabbitmq-subscriber
-kubectl delete svc svc-rabbitmq-subscriber
-kubectl apply -f rabbitmq-secret.yml,svc-rabbitmqsubscriber.yml,rabbitmq-subscriber.yml
+kubectl delete statefulset kern-subscriber critical-subscriber
+kubectl delete svc svc-kern-subscriber svc-critical-subscriber
+kubectl apply -f rabbitmq-secret.yml,svc-rabbitmqsubscriber.yml,rabbitmq-kern-subscriber.yml,rabbitmq-critical-subscriber.yml
