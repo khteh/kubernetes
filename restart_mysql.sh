faf8c6ab2add867e2f250142620a9898295f226e
Teh Kok How <kokhow.teh@4xlabs.co>
2019-10-04 09:36:32 +0800

    Improve delete pvc command using app label

--- restart_mysql.sh
+++ restart_mysql.sh
@@ -3,5 +3,5 @@ kubectl scale sts mysql --replicas=0
 kubectl delete configmap mysqlinit --ignore-not-found=true
 kubectl delete statefulset mysql --ignore-not-found=true
 kubectl delete secret mysql-secret --ignore-not-found=true
-kubectl delete pvc mysql-persistent-storage-mysql-0 --ignore-not-found=true
+kubectl delete pvc -l app=db-mysql
 kubectl apply -f mysql-secret.yml,mysqlinit.yml,mysqlset.yml
