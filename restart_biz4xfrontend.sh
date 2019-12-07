5e59427a0b66bca7ddc34941195e5fe95f9bc52c
Teh Kok How <funcoolgeek@gmail.com>
2019-12-07 17:26:05 +0800

    Delete restart_biz4xfrontend.sh

--- restart_biz4xfrontend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#./restart_env.sh
-kubectl scale sts biz4x-frontend --replicas=0
-kubectl delete secret biz4x-secret --ignore-not-found=true
-kubectl delete statefulset biz4x-frontend
-kubectl delete svc svc-biz4xfrontend
-kubectl apply -f biz4x-secret.yml,svc-biz4xfrontend.yml,biz4x-frontend.yml
