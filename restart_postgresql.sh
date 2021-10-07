6bfcd0ae117dc38aff45bc27e689f4194b7858cd
Kok How, Teh <funcoolgeek@gmail.com>
2021-10-07 10:44:26 +0800

    Update postgresql manifests and restart script

---
+++ restart_postgresql.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+microk8s kubectl scale sts postgresql --replicas=0
+microk8s kubectl delete svc svc-{postgresql,postgresql-nodeport}
+microk8s kubectl delete configmap postgresql-initdb --ignore-not-found=true
+microk8s kubectl delete statefulset postgresql --ignore-not-found=true
+microk8s kubectl delete secret postgresql-secret --ignore-not-found=true
+#microk8s kubectl delete pvc -l app=db-postgresql
+microk8s kubectl apply -f svc-postgresql.yml,postgresql-secret.yml,postgresql-initdb.yml,postgresql.yml
