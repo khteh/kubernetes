2022a8facb118cd81474af9075ee981fd1c4b411
Kok How, Teh <funcoolgeek@gmail.com>
2021-10-07 10:27:37 +0800

    Rename mysqlinit to mysql-initdb

--- restart_mysql_cluster.sh
+++ restart_mysql_cluster.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-kubectl scale sts mysql-slave --replicas=0
-kubectl scale sts mysql-master --replicas=0
-kubectl delete configmap mysqlinit --ignore-not-found=true
-kubectl delete statefulset mysql-slave --ignore-not-found=true
-kubectl delete statefulset mysql-master --ignore-not-found=true
-kubectl delete secret mysql-secret --ignore-not-found=true
-kubectl delete pvc -l app=mysql-master
-kubectl delete pvc -l app=mysql-slave
-kubectl apply -f mysql-secret.yml,mysqlinit.yml,mysql-master.yml,mysql-slave.yml
+microk8s kubectl delete configmap mysql-initdb --ignore-not-found=true
+microk8s kubectl scale sts mysql-{master,slave} --replicas=0
+microk8s kubectl delete svc svc-mysql-{master,slave,nodeport} --ignore-not-found=true
+microk8s kubectl delete sts mysql-{master,slave} --ignore-not-found=true
+microk8s kubectl delete secret mysql-secret --ignore-not-found=true
+microk8s kubectl delete pvc -l app=mysql-master
+microk8s kubectl delete pvc -l app=mysql-slave
+microk8s kubectl apply -f mysql-secret.yml,svc-mysql-cluster.yml,mysql-initdb.yml,mysql-master.yml,mysql-slave.yml
