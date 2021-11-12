004c27507326746530f7c419e04d5dc8abdae31c
Kok How, Teh <funcoolgeek@gmail.com>
2021-11-12 13:43:45 +0800

    Fix AspNetApiStarter manifests after upgrading to ASP.Net 6 using HTTPS connection

--- restart_aspnetapistarter.sh
+++ restart_aspnetapistarter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #./restart_env.sh
-kubectl scale sts aspnetapistarter --replicas=0
-kubectl delete secret aspnetapistarter --ignore-not-found=true
-kubectl delete statefulset aspnetapistarter
-kubectl delete svc svc-aspnetapistarter
-kubectl apply -f aspnetapistarter.mysql.yml,svc-aspnetapistarter.yml,aspnetapistarter.yml
+microk8s kubectl scale sts aspnetapistarter --replicas=0
+microk8s kubectl delete secret aspnetapistarter --ignore-not-found=true
+microk8s kubectl delete sts aspnetapistarter
+microk8s kubectl delete svc svc-{aspnetapistarter,aspnetapistarter-nodeport} --ignore-not-found=true
+microk8s kubectl apply -f aspnetapistarter.mysql.yml,svc-aspnetapistarter.yml,aspnetapistarter.yml
--- restart_fluentd.sh
+++ restart_fluentd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl delete cm fluentd-config access-log-template --ignore-not-found=true
+microk8s kubectl delete cm fluentd-config access-log-template --ignore-not-found=true
 #kubectl delete sts restapi
 #kubectl apply -f fluentd-config.yml,fluentd-tomcat-config.yml,access-log-template.yml,restapi.yml
-kubectl apply -f fluentd-config.yml,access-log-template.yml
+microk8s kubectl apply -f fluentd-config.yml,access-log-template.yml
