638732f5a2db92888e93e4be506c6b1dc2212407
Kok How, Teh <funcoolgeek@gmail.com>
2022-09-21 10:20:39 +0800

    (1) Add PostgreSQL sts, svc (2) Redirect AspNetCoreWebAPI to PostgreSQL

--- restart_aspnetcorewebapi.sh
+++ restart_aspnetcorewebapi.sh
@@ -4,4 +4,4 @@ microk8s kubectl scale sts aspnetcorewebapi --replicas=0
 microk8s kubectl delete secret aspnetcorewebapi --ignore-not-found=true
 microk8s kubectl delete sts aspnetcorewebapi
 microk8s kubectl delete svc svc-{aspnetcorewebapi,aspnetcorewebapi-nodeport} --ignore-not-found=true
-microk8s kubectl apply -f aspnetcorewebapi.mysql.yml,svc-aspnetcorewebapi.yml,aspnetcorewebapi.yml
+microk8s kubectl apply -f aspnetcorewebapi.postgresql.yml,svc-aspnetcorewebapi.yml,aspnetcorewebapi.yml
