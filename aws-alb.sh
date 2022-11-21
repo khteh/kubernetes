1f5ffeb4ece6a49b8e211eddd911c7466be79eea
Kok How, Teh <funcoolgeek@gmail.com>
2022-11-21 13:08:22 +0800

    Add aws lb controller manifest and creation script

---
+++ aws-alb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# https://docs.aws.amazon.com/eks/latest/userguide/aws-load-balancer-controller.html
+curl -o iam_policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.4.5/docs/install/iam_policy.json
+aws iam create-policy --policy-name AWSLoadBalancerControllerIAMPolicy --policy-document file://iam_policy.json
+eksctl utils associate-iam-oidc-provider --cluster kyberlife --approve
+eksctl create iamserviceaccount --cluster=kyberlife --namespace=kube-system --name=aws-lb-controller --role-name "AmazonEKSLoadBalancerControllerRole" --attach-policy-arn=arn:aws:iam::400679711653:policy/AWSLoadBalancerControllerIAMPolicy --approve
+kubectl apply \
+    --validate=false \
+    -f https://github.com/jetstack/cert-manager/releases/download/v1.9.2/cert-manager.yaml
+kubectl apply -f aws-lb-controller.yml,aws-ingress-class.yml
