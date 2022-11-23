08de8ab25097460956d05c14d91cf14f08436264
Kok How, Teh <funcoolgeek@gmail.com>
2022-11-23 09:28:06 +0800

    Fix kubectl command in can_i.sh

--- can_i.sh
+++ can_i.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-microk8s kubectl auth can-i delete secrets --as=system:serviceaccount:default:default -n default
+microk8s.kubectl auth can-i delete secrets --as=system:serviceaccount:default:default -n default
