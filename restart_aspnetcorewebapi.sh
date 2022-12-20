8d3efad00bc8e58b277dc47bc260ff2f955cace0
Kok How, Teh <funcoolgeek@gmail.com>
2022-12-20 09:07:59 +0800

    Add production appsettings

--- restart_aspnetcorewebapi.sh
+++ restart_aspnetcorewebapi.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #./restart_env.sh
 microk8s kubectl scale sts aspnetcorewebapi --replicas=0
+microk8s kubectl delete cm access-log-template fluentd-config appsettings-production --ignore-not-found=true
 microk8s kubectl delete secret aspnetcorewebapi --ignore-not-found=true
 microk8s kubectl delete sts aspnetcorewebapi
 microk8s kubectl delete svc svc-{aspnetcorewebapi,aspnetcorewebapi-nodeport} --ignore-not-found=true
-#microk8s kubectl apply -f aspnetcorewebapi.postgresql.yml,svc-aspnetcorewebapi.yml,aspnetcorewebapi.yml
+microk8s kubectl apply -f aspnetcorewebapi.postgresql.yml,appsettings.Production.yml,svc-aspnetcorewebapi.yml,aspnetcorewebapi.yml
