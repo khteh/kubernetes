dc294bd2d652f08b87af136dcdacd30e1c2bc1c4
Kok How, Teh <funcoolgeek@gmail.com>
2022-12-21 10:52:13 +0800

    Add gmail smtp credentials secret

--- restart_aspnetcorewebapi.sh
+++ restart_aspnetcorewebapi.sh
@@ -2,7 +2,7 @@
 #./restart_env.sh
 microk8s kubectl scale sts aspnetcorewebapi --replicas=0
 microk8s kubectl delete cm access-log-template fluentd-config appsettings-production --ignore-not-found=true
-microk8s kubectl delete secret aspnetcorewebapi --ignore-not-found=true
+microk8s kubectl delete secret aspnetcorewebapi email-credentials --ignore-not-found=true
 microk8s kubectl delete sts aspnetcorewebapi
 microk8s kubectl delete svc svc-{aspnetcorewebapi,aspnetcorewebapi-nodeport} --ignore-not-found=true
-microk8s kubectl apply -f aspnetcorewebapi.postgresql.yml,appsettings.Production.yml,svc-aspnetcorewebapi.yml,aspnetcorewebapi.yml
+microk8s kubectl apply -f aspnetcorewebapi.postgresql.yml,appsettings.Production.yml,email-credentials.yml,svc-aspnetcorewebapi.yml,aspnetcorewebapi.yml
