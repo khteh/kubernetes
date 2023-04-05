431302d3fb4230afa1323d15509c9523a8321c39
Teh Kok How <funcoolgeek@gmail.com>
2023-04-05 14:12:05 +0800

    Fix cluster auto-scaler

--- aws-cluster-autoscaling.sh
+++ aws-cluster-autoscaling.sh
@@ -6,21 +6,15 @@ eksctl create iamserviceaccount \
   --cluster=my-cluster \
   --namespace=kube-system \
   --name=cluster-autoscaler \
-  --attach-policy-arn=arn:aws:iam::ACCOUNT_ID:policy/eksctl-kyberlife-nodegroup-ng-cluster_name-1a-PolicyAutoScaling \
-  --override-existing-serviceaccounts \
-  --approve
-eksctl create iamserviceaccount \
-  --cluster=my-cluster \
-  --namespace=kube-system \
-  --name=cluster-autoscaler \
-  --attach-policy-arn=arn:aws:iam::ACCOUNT_ID:policy/eksctl-kyberlife-nodegroup-ng-cluster_name-1b-PolicyAutoScaling \
+  --attach-policy-arn=arn:aws:iam::ACCOUNT_ID:policy/AmazonEKSClusterAutoscalerPolicy \
   --override-existing-serviceaccounts \
   --approve
 
-microk8s.kubectl apply -f cluster-autoscaler-autodiscover.yml
 microk8s.kubectl annotate serviceaccount cluster-autoscaler \
   -n kube-system \
-  eks.amazonaws.com/role-arn=arn:aws:iam::ACCOUNT_ID:role/AmazonEKSClusterAutoscalerRole
+  eks.amazonaws.com/role-arn=arn:aws:iam::ACCOUNT_ID:role/<arn:aws:iam::<ACCOUNT_ID>:role/eksctl-kyberlife-addon-iamserviceaccount-kub-Role1-xxx>
+microk8s.kubectl apply -f cluster-autoscaler-autodiscover.yml
+
 microk8s.kubectl patch deployment cluster-autoscaler \
   -n kube-system \
   -p '{"spec":{"template":{"metadata":{"annotations":{"cluster-autoscaler.kubernetes.io/safe-to-evict": "false"}}}}}'
