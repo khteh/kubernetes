c88aae47d61b7c92d65f3fe6161e7485b0860288
Kok How, Teh <funcoolgeek@gmail.com>
2023-05-10 16:27:52 +0800

    Remove finalizers of PVC before removing it

--- restart_redis.sh
+++ restart_redis.sh
@@ -3,6 +3,10 @@
 microk8s kubectl scale sts redis-cluster --replicas=0
 microk8s kubectl delete cm redis-cluster-config --ignore-not-found=true
 microk8s kubectl delete sts redis-cluster --ignore-not-found=true
+for pvc in `microk8s kubectl get pvc -l app=redis-cluster | tail -n +2 | cut -d ' ' -f1`; do
+    echo pvc: $pvc
+    microk8s kubectl patch pvc $pvc -p '{"metadata":{"finalizers":null}}'
+done
 microk8s kubectl delete pvc -l app=redis-cluster --ignore-not-found=true
 microk8s kubectl delete job redis-cluster-init --ignore-not-found=true
 microk8s kubectl apply -f redis-cluster-config.yml,redis-cluster.yml,svc-redis-cluster.yml
