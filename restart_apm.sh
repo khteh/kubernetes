b8e04f0f0276e62acebab33ff6f452670f8a7c3f
Teh Kok How <funcoolgeek@gmail.com>
2023-05-16 14:18:37 +0800

    Delete existing APM in restart

--- restart_apm.sh
+++ restart_apm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+microk8s kubectl delete apm kyberlife
 microk8s kubectl delete secret elasticsearch-eck-ca --ignore-not-found=true
 microk8s kubectl get secret kyberlife-es-http-ca-internal -o go-template='{{index .data "tls.crt" | base64decode }}' > elasticsearch-ca.crt
 microk8s kubectl create secret generic elasticsearch-eck-ca --from-file=tls.crt=elasticsearch-ca.crt
