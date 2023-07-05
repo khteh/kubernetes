b10bd5b29bd81dbf24b42b96246aa7f6c06e4c6d
Teh Kok How <funcoolgeek@gmail.com>
2023-07-05 11:32:48 +0800

    Upgrade AWS ALB to 2.5.3

--- aws-alb.sh
+++ aws-alb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # https://docs.aws.amazon.com/eks/latest/userguide/aws-load-balancer-controller.html
 # https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases
-curl -o iam_policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.5.2/docs/install/iam_policy.json
+curl -o iam_policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.5.3/docs/install/iam_policy.json
 aws iam create-policy --policy-name AWSLoadBalancerControllerIAMPolicy --policy-document file://iam_policy.json
 eksctl utils associate-iam-oidc-provider --cluster kyberlife --approve
 eksctl create iamserviceaccount --cluster=kyberlife --namespace=kube-system --name=aws-alb-sa --role-name "AmazonEKSLoadBalancerControllerRole" --attach-policy-arn=arn:aws:iam::400679711653:policy/AWSLoadBalancerControllerIAMPolicy --approve
