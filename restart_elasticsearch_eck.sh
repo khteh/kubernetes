e580592b14f631f2d2381ad32ee3a94a002e63e4
Teh Kok How <funcoolgeek@gmail.com>
2023-08-08 12:50:30 +0800

    Upgrade elastic crd versions

--- restart_elasticsearch_eck.sh
+++ restart_elasticsearch_eck.sh
@@ -12,13 +12,13 @@ kubectl scale sts kyberlife-es-master --replicas=0
 kubectl delete sts kyberlife-es-master --ignore-not-found=true
 kubectl delete secret kyberlife-es-elastic-user --ignore-not-found=true
 kubectl delete secret elasticsearch-secret --ignore-not-found=true
-#kubectl delete pvc -l elasticsearch.k8s.elastic.co/cluster-name=kyberlife
-#pvcs=(elasticsearch-data-kyberlife-es-data-0 elasticsearch-data-kyberlife-es-data-1 elasticsearch-data-kyberlife-es-master-0 elasticsearch-data-kyberlife-es-master-1 elasticsearch-data-kyberlife-es-master-2)
-#for pvc in ${pvcs[@]}; do
-#    kubectl patch pvc $pvc -p '{"metadata":{"finalizers":null}}'
-#done
-#kubectl create -f https://download.elastic.co/downloads/eck/2.5.0/crds.yaml
-#kubectl apply -f https://download.elastic.co/downloads/eck/2.5.0/operator.yaml
+kubectl delete pvc -l elasticsearch.k8s.elastic.co/cluster-name=kyberlife
+pvcs=(elasticsearch-data-kyberlife-es-data-0 elasticsearch-data-kyberlife-es-data-1 elasticsearch-data-kyberlife-es-master-0 elasticsearch-data-kyberlife-es-master-1 elasticsearch-data-kyberlife-es-master-2)
+for pvc in ${pvcs[@]}; do
+    kubectl patch pvc $pvc -p '{"metadata":{"finalizers":null}}'
+done
+kubectl create -f https://download.elastic.co/downloads/eck/2.9.0/crds.yaml
+kubectl apply -f https://download.elastic.co/downloads/eck/2.9.0/operator.yaml
 kubectl delete elasticsearch kyberlife --ignore-not-found=true
 kubectl delete agent fleet-server elastic-agent --ignore-not-found=true
 kubectl apply -f elastic-agent-account.yml,elasticsearch-eck-secret.yml,elasticsearch-secret.yml,elasticsearch-eck-fleet.yml,elasticsearch-eck.yml
