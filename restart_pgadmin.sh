7bd686f7a691bd13b140438dc4e827637c094e2a
Kok How, Teh <funcoolgeek@gmail.com>
2023-08-27 13:39:49 +0800

    Fix pgadmin restart script and add pgadmin pvc manifest

--- restart_pgadmin.sh
+++ restart_pgadmin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 kubectl scale sts pgadmin --replicas=0
-#kubectl delete svc svc-{postgresql,postgresql-nodeport}
+kubectl delete svc svc-pgadmin --ignore-not-found=true
 kubectl delete secret pgadmin-secret --ignore-not-found=true
-kubectl delete statefulset pgadmin --ignore-not-found=true
+kubectl delete sts pgadmin --ignore-not-found=true
 kubectl delete pvc -l app=pgadmin
-kubectl apply -f svc-pgadmin.yml,pgadmin-secret.yml,pgadmin.yml
+kubectl apply -f pgadmin-pvc.yml,svc-pgadmin.yml,pgadmin-secret.yml,pgadmin.yml
