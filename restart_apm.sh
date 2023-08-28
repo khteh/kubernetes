895c554b894ab29e97134d152ab6867e191571e4
Teh Kok How <funcoolgeek@gmail.com>
2023-08-28 11:26:16 +0800

    Create elastic stack TLS certs and mount it on apm server

--- restart_apm.sh
+++ restart_apm.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# Follow https://www.elastic.co/guide/en/apm/guide/current/agent-tls.html to create the elastic-stack self-signed certs
 kubectl delete apm kyberlife
 kubectl delete secret elasticsearch-eck-ca --ignore-not-found=true
 kubectl delete secret elasticsearch-eck-apm-ca --ignore-not-found=true
+kubectl delete secret elastic-stack-tls-certs --ignore-not-found=true
 kubectl get secret kyberlife-es-http-ca-internal -o go-template='{{index .data "tls.crt" | base64decode }}' > elasticsearch-ca.crt
 kubectl get secret kyberlife-apm-http-ca-internal -o go-template='{{index .data "tls.crt" | base64decode }}' > elasticsearch-apm-ca.crt
 kubectl create secret generic elasticsearch-eck-ca --from-file=tls.crt=elasticsearch-ca.crt
 kubectl create secret generic elasticsearch-eck-apm-ca --from-file=tls.crt=elasticsearch-apm-ca.crt
-kubectl apply -f apmserver.yml
+kubectl apply -f elastic-stack-tls-certs.yml,apmserver.yml
