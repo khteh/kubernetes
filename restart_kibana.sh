04843efe7b4ebbb11a58b07e15486a9e9499ecc0
Teh Kok How <funcoolgeek@gmail.com>
2023-08-28 10:28:21 +0800

    Remove unused kibana manifests

--- restart_kibana.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-Don't use this. Use `kubectl apply kibana-eck.yml` instead
-kubectl scale sts kibana --replicas=0
-kubectl delete configmap kibana-config --ignore-not-found=true
-kubectl delete statefulset kibana --ignore-not-found=true
-kubectl delete svc svc-kibana --ignore-not-found=true
-#kubectl apply -f kibana-config.yml,kibana.yml
