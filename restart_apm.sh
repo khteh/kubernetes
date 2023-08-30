30eb10283b543a27236a309e11fd6dfb94b953e2
Kok How, Teh <funcoolgeek@gmail.com>
2023-08-30 11:11:49 +0800

    Fix APM tls certs

--- restart_apm.sh
+++ restart_apm.sh
@@ -2,11 +2,13 @@
 # Follow https://www.elastic.co/guide/en/apm/guide/current/agent-tls.html to create the elastic-stack self-signed certs
 kubectl delete apm kyberlife
 kubectl delete secret elasticsearch-eck-ca --ignore-not-found=true
-kubectl delete secret elasticsearch-eck-apm-ca --ignore-not-found=true
-kubectl delete secret elastic-stack-tls-certs --ignore-not-found=true
+kubectl delete secret apm-internal-cert --ignore-not-found=true
+kubectl delete secret apm-public-cert --ignore-not-found=true
+kubectl get secret kyberlife-apm-http-certs-internal --template='{{index .data "tls.crt" | base64decode }}' > apm-internal.crt
+kubectl get secret kyberlife-apm-http-certs-internal --template='{{index .data "tls.key" | base64decode }}' > apm-internal.key
+kubectl get secret kyberlife-apm-http-certs-public --template='{{index .data "tls.crt" | base64decode }}' > apm-public.crt
 kubectl get secret kyberlife-es-http-ca-internal -o go-template='{{index .data "tls.crt" | base64decode }}' > elasticsearch-ca.crt
-kubectl get secret kyberlife-apm-http-ca-internal -o go-template='{{index .data "tls.crt" | base64decode }}' > elasticsearch-apm-ca.crt
 kubectl create secret generic elasticsearch-eck-ca --from-file=tls.crt=elasticsearch-ca.crt
-kubectl create secret generic elasticsearch-eck-apm-ca --from-file=tls.crt=elasticsearch-apm-ca.crt
-kubectl create secret generic elastic-stack-tls-certs --from-file=localhost.crt=localhost.crt --from-file=localhost.key=localhost.key
+kubectl create secret generic apm-internal-cert --from-file=tls.crt=apm-internal.crt --from-file=tls.key=apm-internal.key
+kubectl create secret generic apm-public-cert --from-file=tls.crt=apm-public.crt
 kubectl apply -f apmserver.yml
