fc353b4bafe5638ad8384cfec97a2d6904bd6c5b
Teh Kok How <funcoolgeek@gmail.com>
2023-09-18 15:12:44 +0800

    Upgrade ALB and cert manager version

--- aws-alb.sh
+++ aws-alb.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 # https://docs.aws.amazon.com/eks/latest/userguide/aws-load-balancer-controller.html
 # https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases
-curl -o iam_policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.6.0/docs/install/iam_policy.json
+curl -o iam_policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.6.1/docs/install/iam_policy.json
 aws iam create-policy --policy-name AWSLoadBalancerControllerIAMPolicy --policy-document file://iam_policy.json
 eksctl utils associate-iam-oidc-provider --cluster kyberlife --approve
-eksctl create iamserviceaccount --cluster=kyberlife --namespace=kube-system --name=aws-alb-sa --role-name "AmazonEKSLoadBalancerControllerRole" --attach-policy-arn=arn:aws:iam::400679711653:policy/AWSLoadBalancerControllerIAMPolicy --approve
-kubectl apply \
-    --validate=false \
-    -f https://github.com/jetstack/cert-manager/releases/download/v1.12.3/cert-manager.yaml
+eksctl create iamserviceaccount --cluster=kyberlife --namespace=kube-system --name=aws-alb-sa --role-name "AmazonEKSLoadBalancerControllerRole" --attach-policy-arn=arn:aws:iam::<account#>:policy/AWSLoadBalancerControllerIAMPolicy --approve
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.13.0/cert-manager.yaml
 kubectl apply -f aws-ingress-class.yml,aws-lb-controller.yml
