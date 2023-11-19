a2b8deb2242712a204cf5df00623e3afb3e90ced
Kok How, Teh <funcoolgeek@gmail.com>
2023-11-19 16:33:36 +0800

    Upgrade ECK to 8.11.1

--- restart_elasticsearch_eck.sh
+++ restart_elasticsearch_eck.sh
@@ -9,7 +9,6 @@ kubectl scale sts kyberlife-es-data --replicas=0
 kubectl delete sts kyberlife-es-data --ignore-not-found=true
 kubectl scale sts kyberlife-es-master --replicas=0
 kubectl delete sts kyberlife-es-master --ignore-not-found=true
-kubectl delete secret kyberlife-es-elastic-user --ignore-not-found=true
 kubectl delete secret elasticsearch-secret --ignore-not-found=true
 kubectl delete pvc -l elasticsearch.k8s.elastic.co/cluster-name=kyberlife
 pvcs=(elasticsearch-data-kyberlife-es-data-0 elasticsearch-data-kyberlife-es-data-1 elasticsearch-data-kyberlife-es-master-0 elasticsearch-data-kyberlife-es-master-1 elasticsearch-data-kyberlife-es-master-2)
@@ -20,4 +19,4 @@ kubectl create -f https://download.elastic.co/downloads/eck/2.10.0/crds.yaml
 kubectl apply -f https://download.elastic.co/downloads/eck/2.10.0/operator.yaml
 kubectl delete elasticsearch kyberlife --ignore-not-found=true
 kubectl delete agent fleet-server elastic-agent --ignore-not-found=true
-kubectl apply -f elastic-agent-account.yml,elasticsearch-eck-secret.yml,elasticsearch-secret.yml,elasticsearch-eck-fleet.yml,elasticsearch-eck.yml
+kubectl apply -f elastic-agent-account.yml,elasticsearch-secret.yml,elasticsearch-eck-fleet.yml,elasticsearch-eck.yml
