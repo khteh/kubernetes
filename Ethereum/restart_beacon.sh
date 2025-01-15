29deb1a7ef49f3a201f681460312f79c6070c2e8
Kok How, Teh <funcoolgeek@gmail.com>
2025-01-15 21:48:13 +0800

    Only manage jwtsecret at GETH

--- Ethereum/restart_beacon.sh
+++ Ethereum/restart_beacon.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-jwtsecret=`openssl rand -hex 32 | tr -d "\n"`
 kubectl scale sts lodestar --replicas=0
-kubectl delete secret jwtsecret --ignore-not-found=true
 kubectl delete cm beacon-config --ignore-not-found=true
 kubectl create secret generic jwtsecret --from-literal=jwtsecret=$jwtsecret
 kubectl delete svc svc-lodestar-beacon --ignore-not-found=true
