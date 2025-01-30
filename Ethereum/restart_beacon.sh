85821f93bcbad1ce4eb9a784706d9c3f7ca3fc67
Kok How, Teh <funcoolgeek@gmail.com>
2025-01-30 15:24:20 +0800

    Fix restart scripts and nodeport service names

--- Ethereum/restart_beacon.sh
+++ Ethereum/restart_beacon.sh
@@ -2,6 +2,6 @@
 kubectl scale sts lodestar --replicas=0
 kubectl delete cm beacon-config --ignore-not-found=true
 kubectl create secret generic jwtsecret --from-literal=jwtsecret=$jwtsecret
-kubectl delete svc svc-lodestar-beacon --ignore-not-found=true
+kubectl delete svc svc-{lodestar-beacon,lodestar-beacon-nodeport} --ignore-not-found=true
 kubectl delete sts lodestar-beacon
 kubectl apply -f lodestar_config.yml,lodestar_beacon.yml,svc-lodestar-beacon.yml
--- Ethereum/restart_validator.sh
+++ Ethereum/restart_validator.sh
@@ -4,6 +4,6 @@ kubectl scale sts lodestar --replicas=0
 kubectl delete secret jwtsecret --ignore-not-found=true
 kubectl delete cm validator-config --ignore-not-found=true
 kubectl create secret generic jwtsecret --from-literal=jwtsecret=$jwtsecret
-kubectl delete svc svc-lodestar-validator --ignore-not-found=true
+kubectl delete svc svc-{lodestar-validator,lodestar-validator-nodeport} --ignore-not-found=true
 kubectl delete sts lodestar-validator
 kubectl apply -f lodestar_config.yml,lodestar_validator.yml,svc-lodestar-validator.yml
