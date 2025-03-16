11d579610bef96bb19dad011cec6f9f6f8b2681c
Kok How, Teh <funcoolgeek@gmail.com>
2025-03-16 13:32:18 +0800

    Fix fluentd-config name and apply access-log-template

--- restart_pythonrestapi.sh
+++ restart_pythonrestapi.sh
@@ -2,5 +2,5 @@
 kubectl scale sts pythonrestapi --replicas=0
 kubectl delete statefulset pythonrestapi --ignore-not-found=true
 kubectl delete svc svc-{pythonrestapi,pythonrestapi-nodeport} --ignore-not-found=true
-kubectl delete cm pythonrestapi pythonrestapi-fluentd-config --ignore-not-found=true
-kubectl apply -f pythonrestapi_config.yml,pythonrestapi-fluentd-config.yml,svc-pythonrestapi.yml,pythonrestapi.yml
+kubectl delete cm access-log-template pythonrestapi pythonrestapi-fluentd-config --ignore-not-found=true
+kubectl apply -f access-log-template.yml,pythonrestapi_config.yml,pythonrestapi-fluentd-config.yml,svc-pythonrestapi.yml,pythonrestapi.yml
--- restart_ragagent.sh
+++ restart_ragagent.sh
@@ -2,5 +2,5 @@
 kubectl scale sts ragagent --replicas=0
 kubectl delete statefulset ragagent --ignore-not-found=true
 kubectl delete svc svc-{ragagent,ragagent-nodeport} --ignore-not-found=true
-kubectl delete cm ragagent ragagent-fluentd-config --ignore-not-found=true
-kubectl apply -f ragagent_config.yml,ragagent-fluentd-config.yml,svc-ragagent.yml,ragagent.yml
+kubectl delete cm access-log-template ragagent ragagent-fluentd-config --ignore-not-found=true
+kubectl apply -f access-log-template.yml,ragagent_config.yml,ragagent-fluentd-config.yml,svc-ragagent.yml,ragagent.yml
