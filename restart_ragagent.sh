7815a928319e309df511dce3fba7cb9ed198ac4a
Kok How, Teh <funcoolgeek@gmail.com>
2025-03-16 13:46:40 +0800

    Add hypercorn logs and fluentd config

--- restart_ragagent.sh
+++ restart_ragagent.sh
@@ -2,5 +2,5 @@
 kubectl scale sts ragagent --replicas=0
 kubectl delete statefulset ragagent --ignore-not-found=true
 kubectl delete svc svc-{ragagent,ragagent-nodeport} --ignore-not-found=true
-kubectl delete cm access-log-template ragagent ragagent-fluentd-config --ignore-not-found=true
-kubectl apply -f access-log-template.yml,ragagent_config.yml,ragagent-fluentd-config.yml,svc-ragagent.yml,ragagent.yml
+kubectl delete cm access-log-template hypercorn-config ragagent ragagent-fluentd-config --ignore-not-found=true
+kubectl apply -f access-log-template.yml,hypercorn_config.yml,ragagent_config.yml,ragagent-fluentd-config.yml,svc-ragagent.yml,ragagent.yml
