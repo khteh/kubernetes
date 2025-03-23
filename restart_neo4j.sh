48db159306043c2d3aab51cc4a27b7a24c28a7d3
Kok How, Teh <funcoolgeek@gmail.com>
2025-03-23 17:05:00 +0800

    Fix Neo4J config map volume mount

--- restart_neo4j.sh
+++ restart_neo4j.sh
@@ -2,7 +2,7 @@
 kubectl scale sts neo4j --replicas=0
 kubectl delete svc svc-{neo4j,neo4j-nodeport}
 kubectl delete configmap neo4j-config --ignore-not-found=true
-kubectl delete statefulset neo4j --ignore-not-found=true
+kubectl delete sts neo4j --ignore-not-found=true
 kubectl delete secret neo4j-secret --ignore-not-found=true
 kubectl delete pvc -l app=db-neo4j
 #kubectl apply -f svc-neo4j.yml,neo4j-secret.yml,neo4j-initdb.yml,neo4j.yml
