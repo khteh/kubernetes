b8d8c918f2767f67fc002c72bf01b9a96df408d2
Kok How, Teh <funcoolgeek@gmail.com>
2025-03-27 14:08:46 +0800

    Update hypercorn and app config. Fix probes by adding httpheader

--- PythonRestAPI/restart_pythonrestapi.sh
+++ PythonRestAPI/restart_pythonrestapi.sh
@@ -3,4 +3,4 @@ kubectl scale sts pythonrestapi --replicas=0
 kubectl delete statefulset pythonrestapi --ignore-not-found=true
 kubectl delete svc svc-{pythonrestapi,pythonrestapi-nodeport} --ignore-not-found=true
 kubectl delete cm access-log-template hypercorn-config pythonrestapi pythonrestapi-fluentd-config --ignore-not-found=true
-#kubectl apply -f ../access_log_template.yml,hypercorn_config.yml,pythonrestapi_config.yml,fluentd_config.yml,svc-pythonrestapi.yml,pythonrestapi.yml
+kubectl apply -f ../access_log_template.yml,hypercorn_config.yml,pythonrestapi_config.yml,fluentd_config.yml,svc-pythonrestapi.yml,pythonrestapi.yml
