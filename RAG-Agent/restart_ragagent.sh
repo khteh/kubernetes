e9b7738edd839aed0f9cd7a13c27dd75afc924ae
Kok How, Teh <funcoolgeek@gmail.com>
2025-03-30 13:22:15 +0800

    Create ragagent-secret in restart script

--- RAG-Agent/restart_ragagent.sh
+++ RAG-Agent/restart_ragagent.sh
@@ -2,8 +2,9 @@
 kubectl scale sts ragagent --replicas=0
 kubectl delete statefulset ragagent --ignore-not-found=true
 kubectl delete svc svc-{ragagent,ragagent-nodeport} --ignore-not-found=true
-kubectl delete secret gcloud-secret --ignore-not-found=true
+kubectl delete secret gcloud-secret ragagent-secret --ignore-not-found=true
 kubectl delete cm access-log-template hypercorn-config ragagent ragagent-fluentd-config --ignore-not-found=true
 cp ~/.config/gcloud/application_default_credentials.json /tmp
 kubectl create secret generic gcloud-secret --from-file=/tmp/application_default_credentials.json
+kubectl create secret generic ragagent-secret --from-file=/usr/src/Python/rag-agent/.env
 kubectl apply -f ../access_log_template.yml,hypercorn_config.yml,ragagent_config.yml,fluentd_config.yml,svc-ragagent.yml,ragagent.yml
