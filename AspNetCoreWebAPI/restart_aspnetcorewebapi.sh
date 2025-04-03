4e461925b631a6bf87eea135c620286243d8db8e
Kok How, Teh <funcoolgeek@gmail.com>
2025-04-03 20:31:15 +0800

    Update fluentd config map names

--- AspNetCoreWebAPI/restart_aspnetcorewebapi.sh
+++ AspNetCoreWebAPI/restart_aspnetcorewebapi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #./restart_env.sh
 kubectl scale sts aspnetcorewebapi --replicas=0
-kubectl delete cm access-log-template fluentd-config appsettings-production appsettings-staging --ignore-not-found=true
+kubectl delete cm access-log-template aspnetcore-fluentd-config appsettings-production appsettings-staging --ignore-not-found=true
 kubectl delete secret aspnetcorewebapi email-credentials --ignore-not-found=true
 kubectl delete sts aspnetcorewebapi
 kubectl delete svc svc-{aspnetcorewebapi,aspnetcorewebapi-nodeport} --ignore-not-found=true
--- restart_gorestapi.sh
+++ restart_gorestapi.sh
@@ -3,5 +3,6 @@
 kubectl scale sts gorestapi --replicas=0
 #kubectl delete secret gorestapi --ignore-not-found=true
 kubectl delete sts gorestapi
+kubectl delete cm access-log-template go-fluentd-config --ignore-not-found=true
 kubectl delete svc svc-{gorestapi,gorestapi-nodeport} --ignore-not-found=true
 kubectl apply -f svc-gorestapi.yml,gorestapi.yml
