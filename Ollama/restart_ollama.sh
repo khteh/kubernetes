471060f331a4761dcd85916ed7f197afc085fdbe
Kok How, Teh <funcoolgeek@gmail.com>
2025-04-04 11:21:06 +0800

    Fix log volumemount; Add OLLAMA_URI ENV

--- Ollama/restart_ollama.sh
+++ Ollama/restart_ollama.sh
@@ -2,5 +2,5 @@
 kubectl scale sts ollama --replicas=0
 kubectl delete cm access-log-template ollama-fluentd-config ollama-config --ignore-not-found=true
 kubectl delete sts ollama
-kubectl delete svc svc-{ollama,ollama-nodeport} --ignore-not-found=true
+#kubectl delete svc svc-{ollama,ollama-nodeport} --ignore-not-found=true
 kubectl apply -f ../access_log_template.yml,fluentd_config.yml,ollama_config.yml,svc-ollama.yml,ollama.yml
