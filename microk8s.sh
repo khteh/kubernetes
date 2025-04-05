2c514a118d04f4535cd42ba893b47bd8043b8d2b
Kok How, Teh <funcoolgeek@gmail.com>
2025-04-05 19:30:04 +0800

    Add gpu addon

--- microk8s.sh
+++ microk8s.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #set -x
-microk8s.enable dns storage ingress dashboard prometheus metrics-server
+microk8s.enable dns gpu storage ingress dashboard prometheus metrics-server
 sudo ufw default allow routed
 microk8s.kubectl config view --raw > ~/.kube/config.microk8s
