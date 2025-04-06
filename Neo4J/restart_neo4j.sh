c8d6f4888def15b77c3eb3ab754d81d04905c49d
Kok How, Teh <funcoolgeek@gmail.com>
2025-04-06 11:34:24 +0800

    Fix component label names of DBs. Add missing school.sql init db

--- Neo4J/restart_neo4j.sh
+++ Neo4J/restart_neo4j.sh
@@ -4,5 +4,5 @@ kubectl scale sts neo4j --replicas=0
 kubectl delete configmap neo4j-config healthcare-neo4j --ignore-not-found=true
 kubectl delete sts neo4j --ignore-not-found=true
 kubectl delete secret neo4j-secret --ignore-not-found=true
-kubectl delete pvc -l app=db-neo4j
+kubectl delete pvc -l app=neo4j
 kubectl apply -f svc-neo4j.yml,neo4j_config.yml,healthcare_data.yml,neo4j_secret.yml,neo4j.yml
--- PostgreSQL/restart_postgresql.sh
+++ PostgreSQL/restart_postgresql.sh
@@ -4,5 +4,5 @@ kubectl delete svc svc-{postgresql,postgresql-nodeport}
 kubectl delete configmap postgresql-initdb --ignore-not-found=true
 kubectl delete statefulset postgresql --ignore-not-found=true
 kubectl delete secret postgresql-secret --ignore-not-found=true
-kubectl delete pvc -l app=db-postgresql
+kubectl delete pvc -l app=postgresql
 kubectl apply -f svc-postgresql.yml,postgresql_secret.yml,postgresql-initdb.yml,postgresql.yml
