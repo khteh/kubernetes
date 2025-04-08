ab611046d8da27a6008980e0f850ea305f53bd94
Kok How, Teh <funcoolgeek@gmail.com>
2025-04-08 17:24:07 +0800

    Add missing PVC deletion in chroma restart script

--- Ollama/restart_ollama.sh
+++ Ollama/restart_ollama.sh
@@ -2,5 +2,6 @@
 kubectl scale sts ollama --replicas=0
 kubectl delete cm access-log-template ollama-fluentd-config ollama-config --ignore-not-found=true
 kubectl delete sts ollama
+kubectl delete pvc -l app=ollama
 #kubectl delete svc svc-{ollama,ollama-nodeport} --ignore-not-found=true
 kubectl apply -f ../access_log_template.yml,fluentd_config.yml,ollama_config.yml,svc-ollama.yml,ollama.yml
